db468c803a5b1398260ef6e9190326a3f7b626ee
Abdullah Basit <abdullah.basit@hotmail.com>
2023-02-23 17:23:26 +0500

    completed home, about and contact page (2)

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import ContactForm from "@/components/contactform";
+
+export default function Page() {
+  return(
+    <div className="flex justify-center m-10">
+      <ContactForm />
+    </div>
+  )
+}
